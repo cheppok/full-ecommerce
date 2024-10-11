917f0eae7e74e6604c7402c4a98260d6ae3c2b74
cheppok <cheppok4real@gmaile.com>
2024-10-12 00:46:33 +0100

    contact page created

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,40 +1,52 @@
+
+'use client'
 import React from 'react'
 import Header2 from '../components/header2';
 import Image from 'next/image';
 import Link from 'next/link'
+import { useState} from 'react'
 
 
-export default function Contact() {
+
+
+const Contact: React.FC = () => {
+  
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
   return (
     <div>
        <Header2/>
-        <div className='h-48'>
+        <div className='h-44 ml-52 flex items-center'>
           Contact
         </div>
             <main className='flex justify-center space-x-6'>
                 <section className=' contactSection1 flex flex-col border-4 border-slate-50'>
+                      <div className='border-b-2 border-gray-400'>
+                          <div className='flex space-x-4 text-xl pb-4'>
+                              <Image src={"/images/icons8-telephone-96.png"} alt='' height={30} width={30} className=' bg-red-600 h-10 w-10 rounded-full flex items-center justify-center text-white '/>
+                              <span>Call To Us</span>
+                              </div>
+                              <p className='text-sm pb-4'>We are available 24/7, 7days a week</p>
+                              <p className='text-sm pb-8'>Phone +2348056219922</p>
+                          </div> 
                       <div>
-                          <div className='flex space-x-4 text-xl'>
-                          <Image src={""} alt='' height={20} width={20}/>
-                          <span>Call To Us</span>
-                          </div>
-                        <p className='text-lg'>We are available 24/7, 7days a week</p>
-                        <p className='text-lg'>Phone +2348056219922</p>
+
+                      <div className='flex space-x-4 text-xl pt-10'>
+                          <Image src={"/images/icons8-envelope-96.png"} alt='' height={20} width={20} className='bg-red-600 h-10 w-10 rounded-full flex items-center justify-center text-white p-1'/>
+                          <span className='text-lg'>Write To Us</span>
                       </div>
-                      <div>
-                        <div className='flex space-x-4 text-xl'>
-                        <Image src={""} alt='' height={20} width={20}/>
-                        <span className='text-lg'>Write To Us</span>
-                        </div>
-                        <p className='text-lg'>Fill out our form and we will contact </p>
-                        <p className='text-lg'>you within 24 hours</p>
+                        <p className='text-sm  '>Fill out our form and we will contact </p>
+                        <p className='text-sm pb-4'>you within 24 hours</p>
                       
-                        <div className='text-lg'>   
+                        <div className='text-sm pb-4'>   
                           <Link href={''}>
                             Emails: cheppok4real@yahoo.com
                           </Link>
                         </div>
-                        <div className='text-lg'>
+                        <div className='text-sm'>
                           <Link href={''}>
                             Emails: cheppok4real@gmail.com
                           </Link>
@@ -42,35 +54,63 @@ export default function Contact() {
                       </div>
                  </section>
 
+
+                 
+
                 <section className='contactSection2'>
                  
-                    <form className="space-y-4 md:space-y-6" action="#">
-                      <section>  
-                            <div className="flex shadow-">         
-                                <div>
-                                  <input type="text" name="name" id="name" className="border-b-2 border-b-gray-300 text-gray-900  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder=" Your Name" required/>
-                                </div>
-                                <div>
-                                  <input type="email" name="email" id="email" className="border-b-2 border-b-gray-300 text-gray-900  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Your Email" required/>
-                                  </div>
-                                  <div>
-                                  <input type="number" name="number" id="phoneNumber" placeholder="Phone Number" className=" border-b-2 border-b-gray-300 text-gray-900  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" required/>
-                                  </div>
+                    <form  action="#">
+                      <section className='space-y-8'>  
+                            <div className="flex justify-between">         
+                              <div className='relative w-1/3 pr-2'>
+                                  <input 
+                                  type="text" name="name" id="name" 
+                                  className="bg-gray-100 text-gray-900  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" 
+                                  placeholder=" Your Name"
+                                  required  
+                                  // this wilmake the red Asterisk change state and disappear when iput changes from placeholder 
+                                  value={inputValue} 
+                                  onChange={handleChange}/>  
+                                   {!inputValue && <span className='text-red-500 absolute right-36 top-2'>*</span>}   
+                              </div>
+
+                              <div className='relative w-1/3 pr-2 pl-2'>
+                                  <input type="email" name="email" id="email" className="bg-gray-100 text-gray-900  w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Your Email" required
+                                  // this wilmake the red Asterisk change state and disappear when iput changes from placeholder 
+                                  value={inputValue} 
+                                  onChange={handleChange}/>  
+                                   {!inputValue && <span className='text-red-500 absolute left-24 top-2'>*</span>}   
+                               </div>
+                                  
+                               <div className='relative w-1/3 pl-2 '>
+                                  <input type="number" name="number" id="phoneNumber" placeholder="Phone Number" className=" bg-gray-100 text-gray-900  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" required
+                                  // this wilmake the red Asterisk change state and disappear when iput changes from placeholder 
+                                  value={inputValue} 
+                                  onChange={handleChange}/>  
+                                   {!inputValue && <span className='text-red-500 absolute right-28 top-2'>*</span>}  
+                               </div>
                             </div>
 
-                            <div>
-                                  <input type="text" name="text" id="message" placeholder="Your Message" className=" border-b-2 border-b-gray-300 text-gray-900  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" required/>
+                            <div >
+                              <textarea name="" id="contactTextarea" placeholder="Your Message" className=" bg-gray-100 text-gray-900 h-52 w-full p-2.5 rounded-sm  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
+                              </textarea>
                             </div>
+                            
+                          
                         </section>
-                        
-                                <button type="submit" className=" bg-red-600 h-11 rounded-md text-white  text-xs text-center dark:bg-primary-600 dark:hover:bg-primary-700         dark:focus:ring-primary-800">Send Message
-                                </button>
+                          <div className='flex justify-end w-full mt-6'>
+                            <button type="submit" className=" bg-red-600 h-14 rounded-md text-white  text-center w-48  dark:bg-primary-600 dark:hover:bg-primary-700         dark:focus:ring-primary-800">Send Message
+                            </button>
+                          </div>
+                               
                                                               
                       </form>
            
-                  </section>      
+                  </section> 
+                       
             </main>
     
     </div>
   )
 }
+ export default Contact
